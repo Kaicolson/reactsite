6bb13da91487d867e2461a2ab6d734bdb236e050
Kaicolson <hello@kaicolson.com>
2021-04-12 20:20:52 +0100

    implemented firebase utils, added sign-in and sign-up authenticated users in firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,34 @@ const config = {
     appId: "1:110674486712:web:243e63c864ce6e41a1db4d"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const  snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+try {
+   await userRef.set({
+     displayName,
+     email,
+     createdAt,
+     ...additionalData
+   })
+  }
+catch (error){
+  console.log('error creating user', error.message);
+}
+
+}
+  return userRef;
+
+};
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
